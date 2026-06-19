f250d630e5da7c07d5f573c895202c7ca51a7cdb
Mark Yacoub <markyacoub@google.com>
2026-06-18 21:58:30 -0400

    chore: Set Astro base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  // Set the base path to match the GitHub repository name
+  site: 'https://mark-yacoub.github.io',
+  base: '/ready_apologia',
   // Since we are doing Scenario A (Static Site), we want 'static' output (default)
   output: 'static',
 });
